ebbe91ce021f44c9fd7c26686fb1614ccb393f73
unknown <ramnvlb@gmail.com>
2018-07-04 11:39:38 +0530

    chnage php file

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+
+<form action="proces.php" method="POST">
+	username:<input type="text" name="username"><br>
+	password:<input type="password" name="pass"><br>
+	<input type="submit" name="" value="submit">
+
+</form>
+
+
+</body>
+</html>
